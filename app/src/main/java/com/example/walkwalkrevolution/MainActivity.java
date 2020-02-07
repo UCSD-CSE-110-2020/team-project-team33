b53f905c46d68dc8f8b52bfdfe0bd801974ca179
Leo <notnow@imbusy.com>
2020-02-06 23:09:54 -0800

    Added tab layout, bug with button sometimes showing up at bottom

--- app/src/main/java/com/example/walkwalkrevolution/MainActivity.java
+++ app/src/main/java/com/example/walkwalkrevolution/MainActivity.java
@@ -1,46 +1,39 @@
 package com.example.walkwalkrevolution;
 
-import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.walkwalkrevolution.fitness.*;
+import com.example.walkwalkrevolution.ui.main.MainFragment;
+import com.example.walkwalkrevolution.ui.main.RoutesFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.walkwalkrevolution.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private String fitnessServiceKey = "GOOGLE_FIT";
+
+    private SectionsPagerAdapter sectionsPagerAdapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnGoToSteps = findViewById(R.id.buttonGoToSteps);
-        btnGoToSteps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchStepCountActivity();
-            }
-        });
+        sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
-            @Override
-            public FitnessService create(StepCountActivity stepCountActivity) {
-                return new GoogleFitAdapter(stepCountActivity);
-            }
-        });
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
 
-        launchStepCountActivity();
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    public void launchStepCountActivity() {
-        Intent intent = new Intent(this, StepCountActivity.class);
-        intent.putExtra(StepCountActivity.FITNESS_SERVICE_KEY, fitnessServiceKey);
-        startActivity(intent);
-    }
-
-    public void setFitnessServiceKey(String fitnessServiceKey) {
-        this.fitnessServiceKey = fitnessServiceKey;
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MainFragment(), getString(R.string.home_tab));
+        adapter.addFragment(new RoutesFragment(), getString(R.string.routes_tab));
+        viewPager.setAdapter(adapter);
     }
 }
