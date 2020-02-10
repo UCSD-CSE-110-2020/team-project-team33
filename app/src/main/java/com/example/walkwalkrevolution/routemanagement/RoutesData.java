85077a713f1a1c64e1a9ed1d55031f7cbba0b168
Leo <notnow@imbusy.com>
2020-02-10 15:48:23 -0800

    Routes are sorted alphabetically in the database and are now iterable

--- app/src/main/java/com/example/walkwalkrevolution/routemanagement/RoutesData.java
+++ app/src/main/java/com/example/walkwalkrevolution/routemanagement/RoutesData.java
@@ -1,52 +1,35 @@
 package com.example.walkwalkrevolution.routemanagement;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
 
-public class RoutesData implements Serializable {
-    private ArrayList<Route> routes;
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class RoutesData implements Serializable, Iterable {
+    private TreeSet<Route> routes;
 
     public RoutesData() {
-        routes = new ArrayList<Route>();
+        routes = new TreeSet<Route>(new Comparator<Route>() {
+            @Override
+            public int compare(Route routes, Route t1) {
+                return routes.getName().compareTo(t1.getName());
+            }
+        });
     }
 
-    public Route getRoute(int index) {
-        return routes.get(index);
-    }
     public void addRoute(Route route) {
         routes.add(route);
-        /*
-        String name = route.getName();
-        int low = 0;
-        int high = routes.size() - 1;
-        int mid = 0;
-        while(low < high){
-            mid = (low + high) / 2;
-            String midName = routes.get(mid).getName();
-            if (midName.compareToIgnoreCase(name) < 0){
-                low = mid + 1;
-                continue;
-            }
-            if(midName.compareToIgnoreCase(name) > 0){
-                high = mid - 1;
-                continue;
-            }
-            break;
-        }
-        routes.add(mid, route);
-         */
     }
 
     public int getNumRoutes() {
         return routes.size();
     }
 
-    public boolean hasRouteName(String name){
-        for(Route route: routes){
-            if (route.getName().equals(name)){
-                return true;
-            }
-        }
-        return false;
+    @NonNull
+    @Override
+    public Iterator iterator() {
+        return routes.iterator();
     }
 }
