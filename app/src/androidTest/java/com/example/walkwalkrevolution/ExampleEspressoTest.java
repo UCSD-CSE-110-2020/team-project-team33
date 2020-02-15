2a7cde7b3a2ff6501979916823381e0a9138fb95
Leo <notnow@imbusy.com>
2020-02-14 22:04:28 -0800

    Wrote tests

--- app/src/androidTest/java/com/example/walkwalkrevolution/ExampleEspressoTest.java
+++ app/src/androidTest/java/com/example/walkwalkrevolution/ExampleEspressoTest.java
@@ -1,6 +1,5 @@
 package com.example.walkwalkrevolution;
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
---
+++ app/src/test/java/com/example/walkwalkrevolution/TestMock.java
@@ -0,0 +1,70 @@
+package com.example.walkwalkrevolution;
+
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.walkwalkrevolution.fitness.FitnessServiceFactory;
+import com.example.walkwalkrevolution.walktracker.WalkInfo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.LooperMode;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+@LooperMode(LooperMode.Mode.PAUSED)
+public class TestMock {
+    private static final String TEST_SERVICE = "TEST_SERVICE";
+
+    private static final int POSATIVE_STEP_COUNT = 237;
+    private static final int NEGATIVE_STEP_COUNT = -5;
+    private static final int VALID_HEIGHT = 63;
+
+    private Intent intent;
+
+    @Before
+    public void setUp() {
+        FitnessServiceFactory.put(TEST_SERVICE, MockFitnessService::new);
+        intent = new Intent(ApplicationProvider.getApplicationContext(), TabActivity.class);
+        intent.putExtra(DataKeys.FITNESS_SERVICE_KEY, TEST_SERVICE);
+        intent.putExtra(DataKeys.USER_HEIGHT_KEY, VALID_HEIGHT);
+        intent.putExtra(DataKeys.ROUTE_MANAGER_KEY, new MockRoutesManager());
+    }
+
+    @Test
+    public void testWalkInfoMocking() {
+        ActivityScenario<TabActivity> scenario = ActivityScenario.launch(intent);
+        scenario.onActivity(activity -> {
+            activity.stepCountFragment.getWalkInfo().setMocking(true);
+            activity.stepCountFragment.getWalkInfo().setSteps(POSATIVE_STEP_COUNT);
+            activity.stepCountFragment.getStepUpdate().update();
+
+            TextView steps = activity.findViewById(R.id.overall_steps);
+            assertThat(steps.getText().toString()).isEqualTo(Long.toString(POSATIVE_STEP_COUNT));
+        });
+    }
+
+    @Test
+    public void testWalkInfoWalkMocking() {
+        ActivityScenario<TabActivity> scenario = ActivityScenario.launch(intent);
+        scenario.onActivity(activity -> {
+            activity.stepCountFragment.getWalkInfo().setMocking(true);
+            activity.stepCountFragment.getWalkInfo().setSteps(POSATIVE_STEP_COUNT);
+            activity.stepCountFragment.getStepUpdate().update();
+            activity.stepCountFragment.getWalkUpdate().update();
+            activity.stepCountFragment.getWalkInfo().startWalk();
+            activity.stepCountFragment.getWalkInfo().setSteps(POSATIVE_STEP_COUNT + POSATIVE_STEP_COUNT);
+
+
+            TextView steps = activity.findViewById(R.id.walk_steps);
+            assertThat(steps.getText().toString()).isEqualTo(Long.toString(POSATIVE_STEP_COUNT));
+        });
+    }
+}
