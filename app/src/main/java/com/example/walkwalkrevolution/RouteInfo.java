fdfd4bb427108422e65d202dfc8cca7694c90288
Will Hwong <38929001+wdhwong@users.noreply.github.com>
2020-02-14 21:14:33 -0800

    Updated route info ui

--- app/src/main/java/com/example/walkwalkrevolution/RouteInfo.java
+++ app/src/main/java/com/example/walkwalkrevolution/RouteInfo.java
@@ -1,14 +1,15 @@
 package com.example.walkwalkrevolution;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RouteInfo extends AppCompatActivity {
 
@@ -19,12 +20,13 @@ public class RouteInfo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button startBttn = findViewById(R.id.route_info_bttn);
+        startBttn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                finish();
             }
         });
     }
